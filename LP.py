a13134b5cec7697927d9c387edcdd468a5cfa8d5
Mud-Fire <537506288@qq.com>
2019-09-25 23:02:49 +0800

    fresh

--- LP.py
+++ LP.py
@@ -1,27 +1,31 @@
 import numpy as np
 
-class LP:
-    Objective = [1, 3, 1, -2]
 
-    ConstraintA = [
+class LpStruct:
+    Objective = np.array([1, 3, 1, -2])
+
+    A = np.array([
         [2, -1, 1, 0],
         [1, 1, 1, 0],
         [1, 0, 0, 1],
         [3, 0, 2, 0],
-    ]
-    ConstraintSign = [0, 0, 0, 0]
-    ConstraintB = [4, 6, 2, 10]
+    ])
+    Sign = np.array([0, 0, 0, 0])
+    B = np.array([4, 6, 2, 10])
 
-    Variable = [1, 1, 1, 1]
-
-
-def StandardLP(LP):
-
-    for i in range(0,len(LP.Variable)):
-        if LP.Variable[i] < 0:
-            break
-    return LP
+    Variable = np.array([1, -1, 0, 1])
 
 
-x = LP()
-print(StandardLP(x))
+def StandardLP(LpStruct):
+    # 根据变量符号进行变量转化
+    for i in range(0, len(LpStruct.Variable)):
+        if LpStruct.Variable[i] < 0:
+            LpStruct.A[:, i] = - LpStruct.A[:, i]
+        elif LpStruct.Variable[i] == 0:
+            LpStruct.A = np.insert(LpStruct.A, i+1, values=(-LpStruct.A[:, i]), axis=1)
+    return LpStruct
+
+
+x = LpStruct()
+x = StandardLP(x)
+print(x.A[:])
