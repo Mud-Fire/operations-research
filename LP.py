0954b728a5e46f36068d6ffa833e7da3e9ba7720
Mud-Fire <537506288@qq.com>
2019-09-26 11:04:34 +0800

    StandardLp Function

--- LP.py
+++ LP.py
@@ -2,17 +2,27 @@ import numpy as np
 
 
 class LpStruct:
-    Objective = np.array([1, 3, 1, -2])
 
+    # 目标函数
+    # 目标函数最大值还是最小值（1，max）（0，min）
+    ObjectSign = 0
+    # 目标函数系数
+    Objective = np.array([3, 1, -2])
+
+    # 约束条件
+    # 约束条件系数矩阵
     A = np.array([
         [2, -1, 1, 0],
         [1, 1, 1, 0],
         [1, 0, 0, 1],
         [3, 0, 2, 0],
     ])
-    Sign = np.array([0, 0, 0, 0])
-    B = np.array([4, 6, 2, 10])
 
+    # 约束条件的符号 (-1, <=) (1, >=) (0, =)
+    Sign = np.array([-1, 0, 0, 0])
+    # 约束条件的右端项
+    B = np.array([-4, 6, 2, 10])
+    # 变量
     Variable = np.array([1, -1, 0, 1])
 
 
@@ -21,11 +31,33 @@ def StandardLP(LpStruct):
     for i in range(0, len(LpStruct.Variable)):
         if LpStruct.Variable[i] < 0:
             LpStruct.A[:, i] = - LpStruct.A[:, i]
+            LpStruct.Objective[i] = - LpStruct.Objective[i]
         elif LpStruct.Variable[i] == 0:
             LpStruct.A = np.insert(LpStruct.A, i+1, values=(-LpStruct.A[:, i]), axis=1)
+            LpStruct.Objective = np.insert(LpStruct.Objective, i+1, values=(-LpStruct.Objective[i]))
+
+    # 根据符号进行标准化转化
+    for i in range(0, len(LpStruct.Sign)):
+        V_N = np.zeros(len(LpStruct.Sign))
+        if LpStruct.Sign[i] == -1:
+            V_N[i] = 1
+            LpStruct.A = np.c_[LpStruct.A, V_N]
+        elif LpStruct.Sign[i] == -1:
+            V_N[i] = -1
+            LpStruct.A = np.c_[LpStruct.A, V_N]
+
+    # 右端项转成非负
+    for i in range(0, len(LpStruct.B)):
+        if LpStruct.B[i] < 0:
+            LpStruct.A[i] = - LpStruct.A[i]
+            LpStruct.B[i] = - LpStruct.B[i]
+
     return LpStruct
 
 
 x = LpStruct()
 x = StandardLP(x)
+print(x.Objective)
 print(x.A[:])
+print(x.B)
+
